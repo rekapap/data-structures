b67cc1c1827089a8076e17b74a2885b2524c7c5c
Reka <rekapap28@gmail.com>
2019-01-16 21:38:33 +0000

    adds push: check stack is not empty after adding an element

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,7 +1,16 @@
 # Stack data structure
 class Stack
+  def initialize
+    @data = []
+  end
+
   # returns true if no elements in the stack
   def empty?
-    true
+    @data.empty?
+  end
+
+  # puts an element on the top of the stack
+  def push(element)
+    @data.push(element)
   end
 end
--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,9 +1,17 @@
 require 'stack'
 
 describe Stack do
+  let(:number1) { 1 }
   describe '#empty?' do
     it 'starts empty' do
       expect(subject.empty?).to eq(true)
+    end
+  end
+
+  describe '#push' do
+    it 'check stack is not empty after adding an element' do
+      subject.push(number1)
+      expect(subject.empty?).to eq(false)
     end
   end
 end
